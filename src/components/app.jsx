ec6d56e7cde8e613363d82525cba816692c1e84b
Zackery Biedinger <zbiedinger@zackerys-mbp-3.lan>
2021-02-23 11:14:05 -0500

    added a book, next and previous button components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,56 @@ import React, { Component } from 'react';
 import TitleBar from './TitleBar/titleBar';
 
 class App extends Component { 
-    state = { }
+    constructor(props) {
+        super(props);
+        this.books = [
+            {title: 'Ready Player One', author: 'Ernest Cline'},
+            {title: 'All the Light We Cannot See', author: 'Anthony Doerr'}, 
+            {title: 'The First and Last Freedom', author: 'Jiddu Krishnamurti'},
+        ];
+        this.state = {
+          bookNumber: 0
+        }
+    }   
+
+    goToNextBook(){
+        let tempBookNumber = this.state.bookNumber; 
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+              tempBookNumber = 0;
+            }
+        this.setState({
+            bookNumber: tempBookNumber
+        }); 
+    }
+
+
+    goToPreviousBook(){
+        let tempBookNumber = this.state.bookNumber; 
+        tempBookNumber--;
+        if(tempBookNumber < 0)
+            tempBookNumber = this.books.length - 1; 
+        this.setState({
+            bookNumber: tempBookNumber
+        }); 
+    }
+
     render() {
         return (
             <div className="container-fluid"> 
                 <TitleBar />
+                <div className="row row-spacer"> 
+                    <div className="col-md-4">
+                        <button onClick={() => this.goToPreviousBook()}>Previous Book</button>
+                    </div>
+                    <div className="col-md-4">
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>                    
+                    </div>
+                    <div className="col-md-4">
+                        <button onClick={() => this.goToNextBook()}>Next Book</button>
+                    </div>
+                </div>
             </div>
         );
     } 
