746655ca68bc917d4b1e457b22bacaaba3778b67
Zackery Biedinger <correct-62668663+zbiedinger@users.noreply.github.com>
2021-02-23 11:51:00 -0500

    fixed the bookviewer component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TitleBar from './TitleBar/titleBar';
+import BookViewer from './BookViewer/bookViewer';
 
 class App extends Component { 
     constructor(props) {
@@ -40,18 +41,8 @@ class App extends Component {
         return (
             <div className="container-fluid"> 
                 <TitleBar />
-                <div className="row row-spacer"> 
-                    <div className="col-md-4">
-                        <button onClick={() => this.goToPreviousBook()}>Previous Book</button>
-                    </div>
-                    <div className="col-md-4">
-                        <h1>{this.books[this.state.bookNumber].title}</h1>
-                        <h4>{this.books[this.state.bookNumber].author}</h4>                    
-                    </div>
-                    <div className="col-md-4">
-                        <button onClick={() => this.goToNextBook()}>Next Book</button>
-                    </div>
-                </div>
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={() => 
+                    this.goToNextBook()} previousBook={() => this.goToPreviousBook()}/>
             </div>
         );
     } 
